a35315165499ec706747e07795f518482699955e
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-17 12:22:03 -0600

    Complete | ThreeArrayProduct

--- ThreeArrayProduct.java
+++ ThreeArrayProduct.java
@@ -1,6 +1,7 @@
 public class ThreeArrayProduct {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {2, 2, 2};
+        System.out.println(a[0]*a[1]*a[2]);
         
     }
 }
