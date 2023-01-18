6d27fdbfff248cce2912ac9e74c521aee302c85c
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-18 11:38:31 -0600

    ImmutableSort | Complete

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,40 @@
+import java.util.Arrays;
+
 public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
-        
+        int[] a = {2, 2, 2};
+        int[] b = {0, 0, 0};
+
+        if ((a[0] > a[1] && a[0] > a[2])&&(a[1]<a[2])) {
+            b[2] = a[0];
+            b[1] = a[2];
+            b[0] = a[1];
+        } else if ((a[0] > a[1] && a[0] > a[2])&&(a[2]<a[1])) {
+            b[2] = a[0];
+            b[1] = a[1];
+            b[0] = a[2];
+        } else if ((a[1] > a[0] && a[1] > a[2])&&(a[0]<a[2])) {
+            b[2] = a[1];
+            b[1] = a[2];
+            b[0] = a[0];
+        } else if ((a[1] > a[0] && a[1] > a[2])&&(a[2]<a[0])) {
+            b[2] = a[1];
+            b[1] = a[0];
+            b[0] = a[2];
+        } else if ((a[2] > a[0] && a[2] > a[1])&&(a[1]<a[0])) {
+            b[2] = a[2];
+            b[1] = a[0];
+            b[0] = a[1];
+        } else if ((a[2] > a[0] && a[2] > a[1])&&(a[0]<a[1])) {
+            b[2] = a[2];
+            b[1] = a[1];
+            b[0] = a[0];
+        } else if (a[0] == a[1] && a[0] == a[2]) {
+            b[2] = a[2];
+            b[1] = a[1];
+            b[0] = a[0];
+        }
+        System.out.println(Arrays.toString(b));
+
     }
 }
